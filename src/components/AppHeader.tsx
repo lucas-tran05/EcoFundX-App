63d6f5cc2c36972eca6708dcc2ce1118a067a88a
lucas-tran05 <trancuong13112005@gmail.com>
2025-04-07 14:30:52 +0700

    Feat (UI): Create Footer and Header componets

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,13 +1,49 @@
 'use client';
-import { Layout, Row, Col, Button, Typography, Space } from "antd";
-import Link from "next/link"; // Import Link from Next.js
 import { FC } from "react";
-
-const { Text } = Typography;
+import Link from "next/link";
+import { Button, Flex, Layout, Menu, Typography} from 'antd';
 
 const AppHeader: FC = () => {
     return (
-        <h1>Header</h1>
+        <Layout.Header style={{ padding: '0 40px', backgroundColor: 'var(--background-primary)' }}>
+            <Flex align="center" justify="space-between">
+                <Flex>
+                    <Typography.Title level={3} style={{ fontWeight: 'bold', margin: 0 }}>
+                        <span style={{ color: 'var(--primary-color)' }}>Ecofund</span>X
+                    </Typography.Title>
+                </Flex>
+
+                <Flex>
+                    <Menu
+                        mode="horizontal"
+                        defaultSelectedKeys={['home']} // Mặc định chọn Home
+                        style={{ width: '100%', border: 'none' }}
+                    >
+                        <Menu.Item key="home">
+                            <Link href="/">Home</Link>
+                        </Menu.Item>
+                        <Menu.Item key="projects">
+                            <Link href="/projects">Projects</Link>
+                        </Menu.Item>
+                        <Menu.Item key="forum">
+                            <Link href="/forum">Forum</Link>
+                        </Menu.Item>
+                        <Menu.Item key="contact">
+                            <Link href="/contact">Contact</Link>
+                        </Menu.Item>
+                    </Menu>
+                </Flex>
+
+                <Flex>
+                    <Button type="primary"  style={{ marginRight: '10px' }}>
+                        <Link href="/register">Register</Link>
+                    </Button>
+                    <Button type="default" >
+                        <Link href="/login">Login</Link>
+                    </Button>
+                </Flex>
+            </Flex>
+        </Layout.Header>
     );
 };
 
